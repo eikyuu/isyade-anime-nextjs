0acbf26a053d39f1a5fea5f9ebff9c07740e74d4
eikyuu <vduguet@laposte.net>
2020-12-11 14:05:17 +0100

    test getstaticprops animeCards

--- components/pages/AnimeCards.js
+++ components/pages/AnimeCards.js
@@ -1,20 +1,32 @@
+import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import animeApi from "../../services/animeApi";
 
-const AnimeCards = () => {
-  const [data, setData] = useState();
+const AnimeCards = ({ article }) => {
+  // const [data, setData] = useState();
 
   const loadData = async () => {
     const response = await animeApi.findAll();
-    setData(response);
+    // setData(response);
   };
 
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  console.log(data[0].attributes);
+  // useEffect(() => {
+  //   loadData();
+  // }, []);
+  // console.log(data.data[0].attributes.canonicalTitle);
+  console.log(article);
   return <h1>test</h1>;
+};
+
+export const getStaticProps = async () => {
+  const result = await fetch("https://kitsu.io/api/edge/anime");
+  const article = await result.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default AnimeCards;
